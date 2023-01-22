5d6487d15d9bf53bf328098414744f3289a7e6eb
Aleksey_Volik <volikjob@gmail.com>
2023-01-22 18:07:26 +0400

    Add BookAuthor model
    Add BookAuthor model
    Add relation between Book and BookAuthor model by ForeignKey on Book model

--- book_tracker/book/models.py
+++ book_tracker/book/models.py
@@ -5,5 +5,13 @@ class Book(models.Model):
     """This model is representing a book"""
 
     title = models.CharField(max_length=60)
-    author = models.CharField(max_length=60)
+    author = models.ForeignKey('BookAuthor', on_delete=models.PROTECT)
     amount = models.IntegerField()
+
+
+class BookAuthor(models.Model):
+    """This model is representing a book`s author"""
+
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    patronymic = models.CharField(max_length=50)
